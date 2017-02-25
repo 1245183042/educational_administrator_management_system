9c575bfe1535dd1eb771c3b715f9016cbd01e507
Administrator <Administrator@PC-20160325DJKC>
2017-02-25 10:51:06 +0800

    修改AdminNoticePane类（添加一个DefaultTableModel）

--- Edu_System/src/com/edu/client/view/admin/AdminNoticePane.java
+++ Edu_System/src/com/edu/client/view/admin/AdminNoticePane.java
@@ -1,10 +1,14 @@
 package com.edu.client.view.admin;
 
+import javax.swing.DefaultCellEditor;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 
 import com.edu.util.DateUtil;
 
@@ -12,7 +16,15 @@ public class AdminNoticePane {
 	private JPanel noticePanel;//放在tabbedPane的公告标签
 	private JScrollPane downScrollPane;//用来装公告表格的面板
 	private JTable NoticeTable;//学生信息表格
+	private DefaultTableModel model;
 	
+	public DefaultTableModel getModel() {
+		return model;
+	}
+	public void setModel(DefaultTableModel model) {
+		this.model = model;
+	}
+
 	public JPanel init(){
 		noticePanel = new JPanel();
 		noticePanel.setLayout(null);
@@ -26,9 +38,12 @@ public class AdminNoticePane {
 		addButton.setBounds(268,55,100,20);
 		
 		//用来装公告表格的面板
-		NoticeTable = new JTable(new Object[][]{{"口","关于配合做好车辆通行年票费补缴工作的通知","学生","admin",DateUtil.getTime()},
-				{"口","开学温馨提示","学生","admin",DateUtil.getTime()}},
-				new Object[]{"口删除","标题","发布对象","发布者","发布时间"});
+		model = new DefaultTableModel(new String[]{"口删除","标题","发布对象","发布者","发布时间"}, 0);
+		NoticeTable = new JTable(model);
+		model.addRow(new String[]{"口","关于配合做好车辆通行年票费补缴工作的通知","学生","admin",DateUtil.getTime()});
+		model.addRow(new String[]{"口","开学温馨提示","学生","admin",DateUtil.getTime()});
+		TableColumn column = NoticeTable.getColumnModel().getColumn(0);
+		column.setCellEditor(new DefaultCellEditor(new JCheckBox()));
 		downScrollPane = new JScrollPane(NoticeTable);
 		downScrollPane.setBounds(50, 150, 1000, 300);
 		
